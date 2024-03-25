2b233ae155d735cb65e948ca30ab2c5ca41da7d1
Radosław Bucki <236507@edu.p.lodz.pl>
2024-03-25 20:41:05 +0100

    Read arguments from json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from alghoritms.RandomForest import RandomForest
 from alghoritms.DecisionTree import DecisionTree
+from helper.ArgumentHelepr import ArgumentHelper
 from helper.ConfusionMatrixHelper import ConfusionMatrixHelper
 from helper.DataCsvHelper import DataCsvHelper
 from helper.DataSplitHelper import DataSplitHelper
@@ -11,15 +12,20 @@ def create_classification_algorithm(algorithm_name, learn_data, test_data):
     if algorithm_name == Algorithm.NAIVE_BAYES:
         return NaiveBayes(learn_data, test_data)
     elif algorithm_name == Algorithm.DECISION_TREE:
-        return DecisionTree(learn_data, test_data, 5, 2)
+        max_depth = ArgumentHelper.get_int_argument("max_depth")
+        min_samples_leaf = ArgumentHelper.get_int_argument("min_samples_leaf")
+        return DecisionTree(learn_data, test_data, max_depth, min_samples_leaf)
     elif algorithm_name == Algorithm.RANDOM_FOREST:
-        return RandomForest(learn_data, test_data, 100, 2)
+        n_estimators = ArgumentHelper.get_int_argument("n_estimators")
+        max_features = ArgumentHelper.get_int_argument("max_features")
+        return RandomForest(learn_data, test_data, n_estimators, max_features)
     else:
         raise NotImplementedError(f"Algorithm {algorithm_name} not found!")
 
 
 if __name__ == '__main__':
-    algorithm = Algorithm.RANDOM_FOREST
+    algorithm = ArgumentHelper.get_enum_argument("algorithm", Algorithm)
+
     labels = ['DERMASON', 'SIRA', 'SEKER']
     splitter = DataSplitHelper(DataCsvHelper.read_csv(labels=labels), 0.3)
 
@@ -27,6 +33,7 @@ if __name__ == '__main__':
     classification.train()
     predicted, expected = classification.predict_test_data()
 
+    print("\n")
     cmh = ConfusionMatrixHelper(expected, predicted, labels)
     cmh.display_confusion_matrix()
     cmh.display_classification_report()
