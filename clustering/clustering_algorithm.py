ee09a25a6a06f15058d2c6a975516a4e12c90aa3
Damian Biskupski <ddamian2057@gmail.com>
2024-04-15 22:06:11 +0200

    package improvement

--- clustering/clustering_algorithm.py
+++ clustering/clustering_algorithm.py
@@ -5,11 +5,17 @@ class ClusteringAlgorithm:
 
     def __init__(self, data, model):
         self.data = data
-        self.start_model = model
         self.model = model
 
     def fit(self):
         data_to_fit = self.data
+        X = data_to_fit.drop('Class', axis=1)
+        y = data_to_fit['Class']
+        labels = self.model.fit_predict(X)
+        return labels, X, y
+
+    def fit(self, data):
+        data_to_fit = data
         X = data_to_fit.drop('Class', axis=1)
         y = data_to_fit['Class']
         labels = self.model.fit_predict(X)
